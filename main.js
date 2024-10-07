3c2f23b0c8a3af0f248813f04eb7dbbcaee5c2ac
luizrando <126567917+luizrando@users.noreply.github.com>
2024-10-07 11:36:08 -0300

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-//Pom---------------------------------------
-function tocaSomPom() { 
-    document.querySelector('#som_tecla_pom').play();
-}
-
-const listaDeTeclas=document.querySelectorAll('.tecla');
